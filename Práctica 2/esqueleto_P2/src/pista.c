c6720c973e5b026b16d5bfc2462db00a80e0cac5
JorgeLópezGómez <Jorge.Lopez32@alu.uclm.es>
2024-03-25 21:53:26 +0100

    Hola

--- Práctica 2/esqueleto_P2/src/pista.c
+++ Práctica 2/esqueleto_P2/src/pista.c
@@ -12,9 +12,13 @@ int main(int argc,char *argv[]){
 	//TODO: Esquema según especificado en la práctica.
 
     // Define variables locales
+    pid_t pid = getpid();
+    int valorEspera;
 
     // Coge semáforos y memoria compartida
-    
+    sem_t *sem_pistas = get_semaphore(PISTAS);
+    int *num_aviones_en_espera = get_shared_mem(AVIONESESPERA);
+
     // Se pone en estado de libre incrementando el número de pistas libres
     while(1){
 
@@ -22,7 +26,9 @@ int main(int argc,char *argv[]){
         printf("Pista [%d] en servicio...\n",pid);
         
         //TODO: Aquí hay que realizar procesos
-        
+        sem_wait(sem_pistas);
+        valorEspera = --(*num_aviones_en_espera);
+
         // Mensaje de pista ocupada
         printf("Pista [%d] ocupada... \n",pid);
         printf("Nº de Aviones en espera: %d\n",valorEspera);
@@ -30,6 +36,7 @@ int main(int argc,char *argv[]){
         // Espera en entre 30..60 segundos
         sleep(rand() % 31 + 30);
         
+        sem_post(sem_pistas);
     }
 
     return EXIT_SUCCESS;
