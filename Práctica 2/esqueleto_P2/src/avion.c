1a2a99a3417ecd8816ae28baaf75ff19f62d4ec2
JorgeLópezGómez <Jorge.Lopez32@alu.uclm.es>
2024-04-12 09:56:02 +0200

    Añadir comentarios al código

--- Práctica 2/esqueleto_P2/src/avion.c
+++ Práctica 2/esqueleto_P2/src/avion.c
@@ -11,39 +11,39 @@ int main(int argc, char *argv[])
 {
 
     // Define variables locales
-    pid_t pid = getpid();
-    srand(pid);
-    int valorEspera = 0;
+    pid_t pid = getpid(); // Obtiene el pid del proceso
+    srand(pid);           // Inicializa el generador de numeros aleatorios
+    int valorEspera = 0;  // Numero de aviones en espera
 
     // Coge semáforos y memoria compartida
-    sem_t *sem_pistas = get_sem(PISTAS);
-    sem_t *sem_aviones = get_sem(AVIONES);
-    sem_t *sem_mutex = get_sem(MUTEXESPERA);
-    int shm_fd = obtener_var(AVIONESESPERA);
+    sem_t *sem_pistas = get_sem(PISTAS);     // Semaforo de pistas
+    sem_t *sem_aviones = get_sem(AVIONES);   // Semaforo de aviones
+    sem_t *sem_mutex = get_sem(MUTEXESPERA); // Semaforo mutex de espera
+    int shm_fd = obtener_var(AVIONESESPERA); // Memoria compartida de aviones en espera
 
     // Espera en entre 30..60 segundos
-    printf("Avion[%d] en camino al aeropuerto\n", pid);
-    sleep(rand() % 31 + 30);
+    printf("Avion[%d] en camino al aeropuerto\n", pid); // Mensaje de avion en camino al aeropuerto
+    sleep(rand() % 31 + 30);                            // Espera entre 30 y 60 segundos
 
     // Aumenta los aviones en espera
-    wait_sem(sem_mutex);
-    consultar_var(shm_fd, &valorEspera);
-    valorEspera++;
-    modificar_var(shm_fd, valorEspera);
-    printf("Nº de Aviones en espera: %d\n", valorEspera);
-    signal_sem(sem_mutex);
+    wait_sem(sem_mutex);                                  // Espera a que el semaforo de espera este libre
+    consultar_var(shm_fd, &valorEspera);                  // Consulta el numero de aviones en espera
+    valorEspera++;                                        // Incrementa el numero de aviones en espera
+    modificar_var(shm_fd, valorEspera);                   // Modifica el numero de aviones en espera
+    printf("Nº de Aviones en espera: %d\n", valorEspera); // Muestra el numero de aviones en espera
+    signal_sem(sem_mutex);                                // Libera el semaforo de espera
 
     // Espera una pista libre
-    printf("Avion [%d] esperando pista libre...\n", pid);
-    wait_sem(sem_pistas);
-    signal_sem(sem_aviones);
+    printf("Avion [%d] esperando pista libre...\n", pid); // Mensaje de avion esperando pista libre
+    wait_sem(sem_pistas);                                 // Espera a que haya una pista libre
+    signal_sem(sem_aviones);                              // Incrementa el numero de aviones en espera
 
     // Comienza el aterrizaje
     printf("Avion [%d] comenzando aterrizaje...\n", pid);
 
     // Espera de 60 segundos
     sleep(60);
-    printf("Avion [%d] aparcado...\n", pid);
+    printf("Avion [%d] aparcado...\n", pid); // Mensaje de avion aparcado
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; // Finaliza el programa
 }
--- Práctica 2/esqueleto_P2/src/pista.c
+++ Práctica 2/esqueleto_P2/src/pista.c
@@ -11,39 +11,39 @@ int main(int argc, char *argv[])
 {
 
     // Define variables locales
-    pid_t pid = getpid();
-    srand(pid);
-    int valorEspera = 0;
+    pid_t pid = getpid(); // Obtiene el PID del proceso actual
+    srand(pid);           // Inicializa el generador de numeros aleatorios
+    int valorEspera = 0;  // Numero de aviones en espera
 
-    // Coge semáforos y memoria compartida
-    sem_t *sem_pistas = get_sem(PISTAS);
-    sem_t *sem_aviones = get_sem(AVIONES);
-    sem_t *sem_mutex = get_sem(MUTEXESPERA);
-    int shm_fd = obtener_var(AVIONESESPERA);
+    // Coge semaforos y memoria compartida
+    sem_t *sem_pistas = get_sem(PISTAS);     // Semaforo de pistas
+    sem_t *sem_aviones = get_sem(AVIONES);   // Semaforo de aviones
+    sem_t *sem_mutex = get_sem(MUTEXESPERA); // Semaforo mutex de espera
+    int shm_fd = obtener_var(AVIONESESPERA); // Memoria compartida de aviones en espera
 
-    // Se pone en estado de libre incrementando el número de pistas libres
+    // Se pone en estado de libre incrementando el numero de pistas libres
     while (1)
     {
 
         // Mensaje de Espera
-        printf("Pista [%d] en servicio...\n", pid);
-        signal_sem(sem_pistas);
-        wait_sem(sem_aviones);
+        printf("Pista [%d] en servicio...\n", pid); // Mensaje de pista en servicio
+        signal_sem(sem_pistas);                     // Incrementa el numero de pistas libres
+        wait_sem(sem_aviones);                      // Espera a que un avion aterrice
 
-        // Espera a que un avión aterrice
-        wait_sem(sem_mutex);
-        consultar_var(shm_fd, &valorEspera);
-        valorEspera--;
-        modificar_var(shm_fd, valorEspera);
-        signal_sem(sem_mutex);
+        // Espera a que un avion aterrice
+        wait_sem(sem_mutex);                 // Espera a que el semaforo de espera este libre
+        consultar_var(shm_fd, &valorEspera); // Consulta el numero de aviones en espera en la memoria compartida de aviones en espera
+        valorEspera--;                       // Decrementa el numero de aviones en espera
+        modificar_var(shm_fd, valorEspera);  // Modifica el numero de aviones en espera en la memoria compartida de aviones en espera
+        signal_sem(sem_mutex);               // Libera el semaforo de espera
 
         // Mensaje de pista ocupada
         printf("Pista [%d] ocupada... \n", pid);
-        printf("Nº de Aviones en espera: %d\n", valorEspera);
+        printf("Nº de Aviones en espera: %d\n", valorEspera); // Muestra el numero de aviones en espera
 
         // Espera en entre 30..60 segundos
         sleep(rand() % 31 + 30);
     }
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; // Finaliza el programa
 }
